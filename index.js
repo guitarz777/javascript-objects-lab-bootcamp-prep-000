77decce70688127509a3fe6d8fc5f5a3d5dfe1bb
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-03 18:42:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value })
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  
+  return Object.assign(obj, {[key] : value})
+}
+
+
+function deleteFromObjectByKey(obj, key){
+  
+  obj = delete obj.key;
+  return obj;
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+  delete object.key;
+  return object;
+}
+
+
+
+  
+  
